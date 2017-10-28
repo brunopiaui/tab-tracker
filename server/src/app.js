d192ef708e76c38ce637e4d2cf115b6e05bc2bd7
Bruno Piaui <brunopiaui@gmail.com>
2017-10-28 15:29:47 -0200

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'hello word'
-    })
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+  })
 })
 
 app.listen(process.env.PORT || 8081)
